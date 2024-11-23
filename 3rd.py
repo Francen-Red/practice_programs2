84caf5c5d71158e66f376f8b381d7ceb0c050c15
Francen-Red <francenvenissered@gmail.com>
2024-11-23 12:30:07 +0800

    Create a program that ask user to input 2 numbers. Print Equal when the numbers are the same

---
+++ 3rd.py
@@ -0,0 +1,19 @@
+while True:
+    while True:
+        try:
+            input_num1 = int(input("Please input first num: "))
+            break
+        except:
+            print("Enter a valid number")
+
+    while True:
+        try:
+            input_num2 = int(input("Please input second num: "))
+            break
+        except:
+            print("Enter a valid number")
+
+    if input_num1 == input_num2:
+        print("Equal")
+    elif input_num1 != input_num2:
+        print("Not equal")
