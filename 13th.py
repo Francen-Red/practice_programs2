73297f05552bb015b7535b8013797b4db565892e
Francen-Red <francenvenissered@gmail.com>
2024-11-23 14:00:02 +0800

    Difference of 10 inputted numbers

---
+++ 13th.py
@@ -0,0 +1,19 @@
+def get10_numbers(number_input):
+    while True:
+        try:
+            return int(input(number_input))
+        except:
+            print("ERROR")
+
+num_1 = get10_numbers("Please input a number: ")
+num_2 = get10_numbers("Please input a number: ")
+num_3 = get10_numbers("Please input a number: ")
+num_4 = get10_numbers("Please input a number: ")
+num_5 = get10_numbers("Please input a number: ")
+num_6 = get10_numbers("Please input a number: ")
+num_7 = get10_numbers("Please input a number: ")
+num_8 = get10_numbers("Please input a number: ")
+num_9 = get10_numbers("Please input a number: ")
+num_10 = get10_numbers("Please input a number: ")
+diff = (num_1 - num_2 - num_3 - num_4 - num_5 - num_6 - num_7 - num_8 - num_9 - num_10)
+print("The difference is:", diff)
