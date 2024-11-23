89f761b6088a3c71b2a33bf5068f0f0e35d4b429
Francen-Red <francenvenissered@gmail.com>
2024-11-23 13:57:07 +0800

    Another way

--- 12th.py
+++ 12th.py
@@ -5,6 +5,15 @@ def get10_numbers(number_input):
         except:
             print("ERROR")
 
-numbers = [get10_numbers("Please input a number: ") for _ in range(10)]
-sum_num = sum(numbers)
-print("The total sum is", sum_num)
+num_1 = get10_numbers("Please input a number: ")
+num_2 = get10_numbers("Please input a number: ")
+num_3 = get10_numbers("Please input a number: ")
+num_4 = get10_numbers("Please input a number: ")
+num_5 = get10_numbers("Please input a number: ")
+num_6 = get10_numbers("Please input a number: ")
+num_7 = get10_numbers("Please input a number: ")
+num_8 = get10_numbers("Please input a number: ")
+num_9 = get10_numbers("Please input a number: ")
+num_10 = get10_numbers("Please input a number: ")
+total_sum = (num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8 + num_9 + num_10)
+print("The sum is:", total_sum)
