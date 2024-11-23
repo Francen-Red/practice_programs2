6d909cff0114e57c69c5bd70f6ca2c6c92824369
Francen-Red <francenvenissered@gmail.com>
2024-11-23 12:38:53 +0800

    Create a program that ask user to input 2 numbers. Print the difference of the two numbers.

---
+++ 6th.py
@@ -0,0 +1,17 @@
+while True:
+    while True:
+        try:
+            input_num1 = int(input("Please input first num: "))
+            break
+        except:
+            print("Enter a valid number")
+
+    while True:
+        try:
+            input_num2 = int(input("Please input second num: "))
+            break
+        except:
+            print("Enter a valid number")
+
+    difference_num = input_num1 - input_num2
+    print("The difference is", difference_num)
